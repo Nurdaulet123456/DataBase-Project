b5e1b9c840ca4a71f12804af88c18b3c72d3124b
MoldirKazbek <200103394@stu.sdu.edu.kz>
2022-05-05 16:55:16 +0600

    New New

--- orcleConnection/connect/admin.py
+++ orcleConnection/connect/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-
+admin.site.register(Articles)
 admin.site.register(Bank_card)
-admin.site.register(Other_offices)
-admin.site.register(Discounts)
+admin.site.register(Customers)
+admin.site.register(Customers_Purchase)
 admin.site.register(Consultants)
+admin.site.register(Discounts)
+admin.site.register(Other_offices)
 admin.site.register(Product_Types)
+admin.site.register(Purchases)
+
