c2e7cdabe1de030f413f52a8c5ead1373e80f4d0
MoldirKazbek <200103394@stu.sdu.edu.kz>
2022-05-11 00:28:49 +0600

    eprst

--- orcleConnection/connect/models.py
+++ orcleConnection/connect/models.py
@@ -3,8 +3,6 @@ from email.policy import default
 from django.db import models
 # Create your models here.
 
-
-
 class Bank_card(models.Model):
     bank_name = models.CharField(max_length=100)
     bank_commission_percent = models.FloatField()
@@ -14,8 +12,7 @@ class Bank_card(models.Model):
 class Other_offices(models.Model):
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    number = models.CharField(max_length=100)
-
+    phone_number = models.CharField(max_length=100)
 
 
 class Discounts(models.Model):
@@ -27,16 +24,49 @@ class Discounts(models.Model):
 class Consultants(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    sales_sum = models.IntegerField()
-    image = models.ImageField(default="https://via.placeholder.com/300x300")
-
-
+    sales_sum = models.IntegerField(default=0)
+    image = models.ImageField(default="img/default-avatar.jpg")
 
 class Product_Types(models.Model):
     type_name = models.CharField(max_length=100)
     amount = models.IntegerField()
-    image = models.ImageField(default="https://via.placeholder.com/300x300")
+    image = models.ImageField(default="zhai.jpg")
+
+class Articles(models.Model):
+    article = models.CharField(max_length=50, primary_key=True)
+    type = models.ForeignKey(Product_Types, on_delete=models.CASCADE)
+    XS = models.IntegerField()
+    S = models.IntegerField()
+    L = models.IntegerField()
+    M = models.IntegerField()
+    price = models.IntegerField()
+    gender = models.CharField(max_length=20)
+
+class Customers(models.Model):
+    gender_choices = (('M', 'Male'),
+                      ('F', 'Female'))
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(choices=gender_choices, max_length=2)
+    bank_card_no = models.CharField(max_length=20, blank=True)
+    expiration_date = models.CharField(max_length=15, blank=True)
+    CVV = models.CharField(max_length=3, blank=True)
+    total_purchases = models.IntegerField(default=0)
+    bank_card = models.ForeignKey(Bank_card, on_delete=models.CASCADE)
+    discount_id = models.CharField(max_length=3, blank=True)
+
+class Customers_Purchase(models.Model):
+    purchase_id = models.IntegerField(primary_key=True)
+    cust = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    consultant = models.ForeignKey(Consultants, on_delete=models.CASCADE)
+    data = models.DateField()
+    total_sum = models.IntegerField()
+
+class Purchases(models.Model):
+    purchase_id = models.IntegerField()
+    article = models.ForeignKey(Articles, on_delete=models.PROTECT)
+    sizes = models.CharField(max_length=5)
+    amount = models.IntegerField()
 
 
-# class 
 
